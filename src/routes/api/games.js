5c5cbf3a72017b8fca5d62b26f326d20e08dc081
charliefbowers@gmail.com <charliefbowers@gmail.com>
2019-12-20 09:48:27 -0500

    added try/catch for user and game routes

--- src/routes/api/games.js
+++ src/routes/api/games.js
@@ -3,21 +3,33 @@ const games = require('../../util/games')
 
 
 router.get('/shared-appIDs', async (req, res) => {
-    const steamIDs = req.query.steamIDs.split(',')
-    const sharedAppIDs = await games.getSharedGames(steamIDs)
-    res.send(sharedAppIDs)
+    try {
+        const steamIDs = req.query.steamIDs.split(',')
+        const sharedAppIDs = await games.getSharedGames(steamIDs)
+        res.send(sharedAppIDs)
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 router.get('/shared', async (req, res) => {
-    const steamIDs = req.query.steamIDs.split(',')
-    const platforms = (req.query.platforms) ? req.query.platforms.split(',') : []
-    const sharedGames = await games.getSharedGames(steamIDs)
-    const filteredGames = games.filterGames(sharedGames, { platforms })
-    res.send(filteredGames)
+    try {
+        const steamIDs = req.query.steamIDs.split(',')
+        const platforms = (req.query.platforms) ? req.query.platforms.split(',') : []
+        const sharedGames = await games.getSharedGames(steamIDs)
+        const filteredGames = games.filterGames(sharedGames, { platforms })
+        res.send(filteredGames)
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 router.get('/:appID', async (req, res) => {
-    res.send(await games.getGame(req.params.appID))
+    try {
+        res.send(await games.getGame(req.params.appID))
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -3,26 +3,42 @@ const users = require('../../util/users')
 
 
 router.get('/:steamID', async (req, res) => {
-    const user = await users.getUser(req.params.steamID)
-    res.send(user)
+    try {
+        const user = await users.getUser(req.params.steamID)
+        res.send(user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 router.get('/:steamID/appIDs', async (req, res) => {
-    const user = await users.getUser(req.params.steamID)
-    res.send(user.appIDs)
+    try {
+        const user = await users.getUser(req.params.steamID)
+        res.send(user.appIDs)
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 router.get('/:steamID/friendIDs', async (req, res) => {
-    const user = await users.getUser(req.params.steamID)
-    res.send(user.friends)
+    try {
+        const user = await users.getUser(req.params.steamID)
+        res.send(user.friends)
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 router.get('/:steamID/friends', async (req, res) => {
-    const user = await users.getUser(req.params.steamID)
-    const detailedFriends = await Promise.all(
-        user.friends.map(friend => users.getUser(friend))
-    )
-    res.send(detailedFriends)
+    try {
+        const user = await users.getUser(req.params.steamID)
+        const detailedFriends = await Promise.all(
+            user.friends.map(friend => users.getUser(friend))
+        )
+        res.send(detailedFriends)
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 module.exports = router
