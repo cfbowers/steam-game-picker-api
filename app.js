88c34aec1cd9c80e95fe08f0b31b05d27d8d1cda
charliefbowers@gmail.com <charliefbowers@gmail.com>
2019-12-19 21:00:05 -0500

    removed www, index.js, cleaned up app.js

--- app.js
+++ app.js
@@ -1,37 +1,17 @@
-var createError = require('http-errors')
-var express = require('express')
-var cookieParser = require('cookie-parser')
-var cors = require('cors')
+const express = require('express')
+const cors = require('cors')
+const gamesApiRouter = require('./routes/api/games')
+const usersApiRouter = require('./routes/api/users')
 
-var indexRouter = require('./routes/index')
-var gamesApiRouter = require('./routes/api/games')
-var usersApiRouter = require('./routes/api/users')
 
-var app = express()
+const app = express()
+const port = process.env.PORT || 3001
 
-//Why did this fix the issue with the fe posting and getting the user resources? 
-app.use(cors())
+app.use(cors()) //Why did this fix the issue with the fe posting and getting the user resources? 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
 
-app.use('/', indexRouter)
 app.use('/api/games', gamesApiRouter)
 app.use('/api/users', usersApiRouter)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	next(createError(404))
-})
-
-// error handler
-app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message
-	res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-	res.status(err.status || 500)
-	res.send({error: 'unable to retrieve resource'})
-})
-
-module.exports = app
+app.listen(port, () => console.log('Server is running on port: ', port))
