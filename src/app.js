2175a8ef20462800bfdfee26c1712fc3fff50ed1
charliefbowers@gmail.com <charliefbowers@gmail.com>
2019-12-28 13:34:22 -0500

    clean up of steam routes

--- src/app.js
+++ src/app.js
@@ -9,8 +9,7 @@ const port = process.env.PORT || 3001
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/games', require('./routes/game'))
-app.use('/steam-users', require('./routes/steamUser'))
+app.use('/steam', require('./routes/steam'))
 app.use('/users', require('./routes/user'))
 app.use('/profile', require('./routes/profile'))
 app.use('/auth', require('./routes/auth'))
---
+++ src/routes/steam.js
@@ -0,0 +1,33 @@
+const router = require('express').Router()
+const users = require('../util/steamUsers')
+
+
+router.get('/profile', async (req, res) => {
+    try {
+        //Get user's steam profile data 
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+router.get('/friends', async (req, res) => {
+    try {
+        //show user's friends -- full list 
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+
+router.get('/shared-apps', async (req, res) => {
+    try {
+        //With no query params it should should show shared apps with individual friends 
+        //e.g. { friend: 'steamID', sharedApps: [1,2,3] }
+        //With steamIDs query param it should show just the shared appIDs between the logged in user and the steamIDs provided 
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+
+module.exports = router
