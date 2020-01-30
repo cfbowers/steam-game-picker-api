9db385a05d786b5124eaa56f7e005d2d738960e1
charliefbowers@gmail.com <charliefbowers@gmail.com>
2020-01-30 09:24:28 -0500

    updates to the update profile route

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,7 @@
 //Router for getting and modifying the logged in user's profile
 const router = require('express').Router()
 const auth = require('../middleware/auth')
+const bcrypt = require('bcryptjs')
 
 router.use(auth)
 
@@ -14,8 +15,25 @@ router.get('/', async (req, res) => {
 
 router.patch('/', async (req, res) => {
   try {
+    if (req.user.email === 'demo@demo.com')
+      throw new Error ('you cannot make changes for the demo profile')
+
     const updatedParams = Object.keys(req.body)
-    updatedParams.forEach(param => req.user[param] = req.body[param])
+
+    if (updatedParams.includes('password')) {
+      const currentPasswordMatch = await bcrypt.compare(req.body.password, req.user.password)
+
+      if (!currentPasswordMatch)
+        throw new Error('current password value was not correct')
+
+      if (!(req.body['newPassword'] === req.body['confirmNewPassword']))
+        throw new Error('new password values did not match')
+
+      req.user.password = req.body['newPassword']
+    } else {
+      updatedParams.forEach(param => req.user[param] = req.body[param])
+    }
+
     await req.user.save()
     res.send(req.user)
   } catch (e) {
