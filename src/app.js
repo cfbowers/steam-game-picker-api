6e5e3b16da9abbc0ac5ac9bd27ec99a2db0971eb
charliefbowers@gmail.com <charliefbowers@gmail.com>
2020-03-10 16:44:00 -0400

    add semis to app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,21 @@
-require('./data/mongoose')
-const express = require('express')
-const cors = require('cors')
+require('./data/mongoose');
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
-const port = process.env.PORT || 3001
-
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cors())
-
-app.use('/steam', require('./routes/steam'))
-app.use('/steam/auth', require('./routes/steamAuth'))
-app.use('/auth', require('./routes/auth'))
-app.use('/profile', require('./routes/profile'))
-app.use('/users', require('./routes/user'))
+const app = express();
+const port = process.env.PORT || 3001;
 
 
-app.listen(port, () => console.log('Server is running on port: ', port))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use('/steam', require('./routes/steam'));
+app.use('/steam/auth', require('./routes/steamAuth'));
+app.use('/auth', require('./routes/auth'));
+app.use('/profile', require('./routes/profile'));
+app.use('/users', require('./routes/user'));
+
+const server = app.listen(port, () => console.log('Server is running on port: ', port));
+
+module.exports = server; 
