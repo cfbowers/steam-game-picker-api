469e17469c97f76d9e86472c8d9ec7dd518cce03
charliefbowers@gmail.com <charliefbowers@gmail.com>
2020-03-23 11:56:01 -0400

    trim app.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,20 @@
-/* eslint-disable no-undef */
 require('dotenv').config(); 
 require('./data/mongoose');
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
 const d = require('debug')('steam-roulette-api:server');
-const errorHandler = require('./middleware/errorHandler');
+const ex = require('express');
+const cors = require('cors');
+const routes = require('./routes/index');
+const errors = require('./middleware/errorHandler'); 
 
 
-const app = express();
+const app = ex();
 const port = process.env.PORT || 3001;
 
-app.use(morgan('tiny', { skip: () => !process.env.DEBUG }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(ex.json());
+app.use(ex.urlencoded({ extended: false }));
 app.use(cors());
-app.use('/', require('./routes/index'));
-app.use(errorHandler);
+app.use('/', routes);
+app.use(errors);
 
 
 exports.server = app.listen(port, () => d(`running on ${port}`));
