3a1478446c874f0ff43851d62c92b6923c3cb4dc
charliefbowers@gmail.com <charliefbowers@gmail.com>
2020-04-09 13:52:00 -0400

    error handler more readable + stack included in debug

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,8 +1,19 @@
 const { error } = require('jsend');
 
 module.exports = function (err, _req, res, _next) {
-  if (err.error) err = err.error; // look for errors coming from /utils/errors
-  const message = (typeof err === 'string') ? err : err.message; 
-  const status = (err.status && typeof err.status === 'number') ? err.status : 500; 
-  res.status(status).send(error(message)); 
+  if (err.error) 
+    err = err.error; // look for errors coming from /utils/errors
+
+  const status = (err.status && typeof err.status === 'number') 
+    ? err.status 
+    : 500; 
+    
+  let details = (typeof err === 'string') 
+    ? err 
+    : err.message; 
+
+  if(process.env.DEBUG && err.stack) 
+    details = { message: details, stack: err.stack }; 
+
+  res.status(status).send(error(details)); 
 }
